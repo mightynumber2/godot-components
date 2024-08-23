5e4bb85a315223ce2a02764f12fa402f717b159d
DVo <83097985+mightynumber2@users.noreply.github.com>
2024-08-22 21:10:18 -0700

    Add health + damage handling to slime & projectile
    - Added damage variable and detection function to Projectile.
    - Converted Slime to a scene with adjustable health.
    - Added progress bar to track health and a function to handle damage in Slime.

--- projectile.gd
+++ projectile.gd
@@ -1,10 +1,13 @@
 extends Sprite2D
 
 @export var speed: float = 400
+@export var damage: float = 25
 
 func _physics_process(delta: float) -> void:
 	global_position += Vector2(speed * delta, 0)
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("I hit something!")
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+		queue_free()
--- slime.gd
+++ slime.gd
@@ -1,7 +1,22 @@
 extends CharacterBody2D
 
 @export var speed: float = 200
+@export var health: float = 100
+
+@onready var progress_bar: ProgressBar = $ProgressBar
+
+func _ready() -> void:
+	progress_bar.max_value = health
+	progress_bar.value = health
 
 func _physics_process(delta: float) -> void:
 	velocity = Vector2(-speed, 0)
 	move_and_slide()
+
+func take_damage(damage: float):
+	health -= damage
+	progress_bar.value = health
+	print(damage)
+	
+	if health <= 0:
+		queue_free()
