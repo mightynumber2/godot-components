cbe2e7180536c8239aa9bb51c1a6ff2e61d8644b
DVo <83097985+mightynumber2@users.noreply.github.com>
2024-08-22 19:46:31 -0700

    Add collision detection to slime & projectile
    - Added collision detection for Slime node via CharacterBody2D.
    - Added collision detection to Projectile node using Area2D via a signal.

--- projectile.gd
+++ projectile.gd
@@ -4,3 +4,7 @@ extends Sprite2D
 
 func _physics_process(delta: float) -> void:
 	global_position += Vector2(speed * delta, 0)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("I hit something!")
