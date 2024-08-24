0926cc9d1656f2f51525b80adcf281a52bffcc05
DVo <83097985+mightynumber2@users.noreply.github.com>
2024-08-23 18:00:11 -0700

    Add game over logic and group updates
    - Introduced a new group 'destroyed_on_game_over' for nodes
    - Nodes in this group are cleaned up on game over
    - Added game over label and visibility logic
    - Adjusted 'maximum_units_through' from 5 to 2
    - Added new instances and groups to various scenes
    - Updated global group definitions in project settings

--- game.gd
+++ game.gd
@@ -2,7 +2,10 @@ extends Node
 
 @onready var score_label: Label = $CanvasLayer/ScoreLabel
 @onready var slime_through_label: Label = $CanvasLayer/SlimeThroughLabel
-@export var maximum_units_through: int = 5
+@onready var game_over_label: Label = $CanvasLayer/GameOverLabel
+
+@export var maximum_units_through: int = 2
+
 
 var points: int = 0
 
@@ -12,6 +15,12 @@ func _ready() -> void:
 func _on_slime_detector_body_entered(body: Node2D) -> void:
 	maximum_units_through -= 1
 	slime_through_label.text = str(maximum_units_through)
+	
+	if maximum_units_through <= 0:
+		game_over_label.visible = true
+		var to_destroy = get_tree().get_nodes_in_group("destroyed_on_game_over")
+		for item in to_destroy:
+			item.queue_free()
 
 func _on_slime_died():
 	points += 1
